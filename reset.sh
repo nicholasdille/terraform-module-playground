e8524405d82b8f797cc1218a0923c2c7d32d47d9
Nicholas Dille <nicholas.dille@mailbox.org>
2025-01-24 15:23:41 +0100

    Added reset script

--- reset.sh
+++ reset.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
+set -o errexit
 
 : "${ZONE:=$1}"
 if test -z "${ZONE}"; then
-  echo "Usage: $0 <zone> <name>"
-  exit 1
+    echo "Usage: $0 <zone> <name>"
+    exit 1
 fi
 
 : "${NAME:=$2}"
 if test -z "${NAME}"; then
-  echo "Usage: $0 <zone> <name>"
-  exit 1
+    echo "Usage: $0 <zone> <name>"
+    exit 1
 fi
 
-hcloud server list --selector=purpose="${NAME}"
-hcloud ssh-key list --selector=purpose="${NAME}"
+if test -z "${HETZNERDNS_TOKEN}"; then
+    echo "ERROR: HETZNERDNS_TOKEN is not set"
+    exit 1
+fi
 
-curl -sSfH "Auth-API-Token: $(pp hetzner-dns-web)" "https://dns.hetzner.com/api/v1/zones" \
-| jq --raw-output --arg zone inmylab.de '.zones[] | select(.name == $zone) | .id' \
+hcloud server list --selector=purpose=${NAME} --output=json \
+| jq '.[].id' \
 | xargs -I{} \
-    curl -sSfH "Auth-API-Token: $(pp hetzner-dns-web)" "https://dns.hetzner.com/api/v1/records?zone_id={}" \
-| jq --arg name playground2 '.records[] | select(.name | endswith($name))'
+    hcloud server delete {}
+
+hcloud ssh-key list --selector=purpose=${NAME} --output=json \
+| jq '.[].id' \
+| xargs -I{} \
+    hcloud ssh-key delete {}
+
+curl -sSfH "Auth-API-Token: ${HETZNERDNS_TOKEN}" "https://dns.hetzner.com/api/v1/zones" \
+| jq --raw-output --arg zone "${ZONE}" '.zones[] | select(.name == $zone) | .id' \
+| xargs -I{} \
+    curl -sSfH "Auth-API-Token: ${HETZNERDNS_TOKEN}" "https://dns.hetzner.com/api/v1/records?zone_id={}" \
+| jq --raw-output '.records[] | select(.name == "minecraft") | .id' \
+| xargs -I{} \
+    curl -sSfX DELETE -H "Auth-API-Token: ${HETZNERDNS_TOKEN}" "https://dns.hetzner.com/api/v1/records/{}"
